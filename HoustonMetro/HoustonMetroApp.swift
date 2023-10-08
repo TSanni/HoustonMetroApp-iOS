1cf40ecdc623e22005d62be05bfa8a9453e9f8a2
TSanni <tomas.sanni@gmail.com>
2023-10-08 09:50:39 -0500

    Initial Commit

---
+++ HoustonMetro/HoustonMetroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoustonMetroApp.swift
+//  HoustonMetro
+//
+//  Created by Tomas Sanni on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HoustonMetroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
