1ab43f92b4b46ea8c2c6272aa6007169b6147495
TSanni <tomas.sanni@gmail.com>
2023-10-27 03:04:20 -0500

    noInternetView code update

--- HoustonMetro/Views/NoInternetView.swift
+++ HoustonMetro/Views/NoInternetView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct NoInternetView: View {
-    @State private var rotate: Bool = false
+    @State private var startBlink: Bool = false
     var body: some View {
         VStack {
             Text("The app is not connected to the internet. To view metro details, check your internet connection, then try again.")
                 .multilineTextAlignment(.center)
             Image(systemName: "wifi.exclamationmark")
                 .font(.largeTitle)
-                .opacity(rotate ? 0 : 1)
+                .opacity(startBlink ? 0 : 1)
                 .scaleEffect(2)
                 .padding()
 
@@ -23,7 +23,7 @@ struct NoInternetView: View {
         .padding()
         .onAppear {
             withAnimation(.easeInOut(duration: 0.5).repeatForever()) {
-                rotate = true
+                startBlink = true
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
